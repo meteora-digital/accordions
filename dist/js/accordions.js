3ee3b06d05303618beb8d474c01dc83f758b88a8
Emmanuel <emmanuel@toast.co.nz>
2021-06-14 10:35:05 +1200

    rebuilt to provide huge improvements, better reliability, better performance and no doubt breaking changes

--- dist/js/accordions.js
+++ dist/js/accordions.js
@@ -5,78 +5,79 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
+var _tween = _interopRequireDefault(require("@meteora-digital/tween"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-/* -------------------------------------------------
-Accordion class to handle the opening/closing
-of any accoridon content!
-------------------------------------------------- */
+/*--------------------------------------------------------------------------------
+Accordion functionality
+--------------------------------------------------------------------------------*/
 var Accordion = /*#__PURE__*/function () {
-  function Accordion(block) {
+  function Accordion(section) {
     var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
     _classCallCheck(this, Accordion);
 
-    // The block is the whole AccordionBlock container
-    this.block = block; // Here we can add some custom settings!
+    // The parent container
+    this.section = section; // We will store the accordion items here
+
+    this.items = []; // The user settings / defaults
 
     this.settings = {
-      "class": 'js-accordion',
-      duration: 300,
-      //ms
-      closeOthers: true
-    }; // ObjectAssign all the user's options
+      multiple: false
+    }; // Object assign the settings
 
     for (var key in this.settings) {
-      // Just check if the key exists in the user's options and if it does override the defaults
-      if (options[key] != undefined) this.settings[key] = options[key];
-    } // Initialise the accordion block
+      if (this.settings.hasOwnProperty(key) && options.hasOwnProperty(key)) this.settings[key] = options[key];
+    }
+  } // This will set up an accoridon item and place it into the items array
 
-
-    this.init();
-  }
 
   _createClass(Accordion, [{
-    key: "init",
-    value: function init() {
+    key: "add",
+    value: function add(element) {
       var _this = this;
 
-      // Ideally we want the elements in an array, not a nodeList
-      this.AccordionItems = []; // Find the item elements. This will be found using this.settings.class followed by --item
+      var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-      this.elements = this.block.querySelectorAll(".".concat(this.settings["class"], "--item")) || false; // Loop the nodeList and push each element inside of an object to the items array
+      if (element.nodeType) {
+        var Item = new AccordionItem(element, options); // If we have a trigger and a target and they are both elements
 
-      for (var i = 0; i < this.elements.length; i++) {
-        this.AccordionItems.push(new AccordionItem(this.elements[i], this.settings));
-      } // If we wanna close the others when an item opens
+        if (Item.trigger && Item.trigger.nodeType && Item.target && Item.target.nodeType) {
+          // Add a click event to the item
+          Item.trigger.addEventListener('click', function (e) {
+            e.preventDefault(); // If the item is active
+
+            if (Item.active) {
+              // Close it
+              Item.close();
+            } // Otherwise open it, and maybe close the rest
+            else {
+                // If we dont want multiple open at once, close the other items
+                if (_this.settings.multiple != true) {
+                  _this.items.filter(function (i) {
+                    return i != Item && i.active;
+                  }).forEach(function (i) {
+                    return i.close();
+                  });
+                } // Open the item
 
 
-      if (this.settings.closeOthers) {
-        // For each accordion item
-        this.AccordionItems.forEach(function (AccordionItem) {
-          try {
-            // Add a click handler
-            AccordionItem.trigger.addEventListener('click', function (e) {
-              e.preventDefault(); // Loop all open accordions (not this one!) and close them
+                Item.open();
+              }
+          }); // Slap this AccordionItem into the items array
 
-              _this.AccordionItems.filter(function (item) {
-                return item.data.open && item.trigger != AccordionItem.trigger;
-              }).forEach(function (item) {
-                item.item.classList.remove("".concat(_this.settings["class"], "--open"));
-                item.data.open = false;
-                item.close();
-              });
-            });
-          } catch (err) {
-            // We'll probably reach here if we don't have a trigger element
-            console.log(err);
-          }
-        });
+          this.items.push(Item);
+        }
       }
+
+      ;
     }
   }]);
 
@@ -86,120 +87,89 @@ var Accordion = /*#__PURE__*/function () {
 exports["default"] = Accordion;
 
 var AccordionItem = /*#__PURE__*/function () {
-  function AccordionItem(item, settings) {
+  function AccordionItem(element) {
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
     _classCallCheck(this, AccordionItem);
 
-    this.item = item; // Our accordion settings
+    // Grab the element
+    this.element = element; // This will store some height data for us
 
-    this.settings = settings; // Some settings for the item
+    this.height = {}; // The animation controller
 
-    this.data = {
-      open: false,
-      // We'll use this for the animations
-      height: {
-        current: 0,
-        full: 0
-      }
-    }; // Used for fps control
+    this.controller = new _tween["default"](); // The user and default settings
 
-    this.time = {
-      previous: Date.now(),
-      current: Date.now(),
-      elapsed: 0,
-      interval: 16.6666666667 // (1000ms / 60 = 16.6666666667ms) = 60fps
+    this.settings = {
+      active: false,
+      trigger: this.element.firstElementChild,
+      target: this.element.lastElementChild
+    }; // Object assign the settings
 
-    };
-    this.init();
+    for (var key in this.settings) {
+      if (this.settings.hasOwnProperty(key) && options.hasOwnProperty(key)) this.settings[key] = options[key];
+    } // The element that we will interact with
+
+
+    this.trigger = this.settings.trigger; // The element that will open / close
+
+    this.target = this.settings.target; // An active state for the accordion item
+
+    this.active = this.settings.active; // Remove the settings because we dont need them now
+
+    delete this.settings; // Initialise the styles
+
+    if (this.active) this.element.classList.add('active');
+    this.target.style.height = this.active ? 'auto' : '0px';
   }
 
   _createClass(AccordionItem, [{
-    key: "init",
-    value: function init() {
-      var _this2 = this;
-
-      // Find the trigger element
-      this.trigger = this.item.querySelector(".".concat(this.settings["class"], "--trigger")) || false; // Find the target element
-
-      this.target = this.item.querySelector(".".concat(this.settings["class"], "--target")) || false; // If we have both a trigger and a target
-
-      if (this.trigger && this.target) {
-        // When we click the trigger
-        this.trigger.addEventListener('click', function (e) {
-          e.preventDefault(); // Set the item's current height to it's current height
-
-          _this2.data.height.current = _this2.target.offsetHeight; // Set the target's height to auto!
-
-          _this2.target.style.height = 'auto'; // Save the target's height
-
-          _this2.data.height.full = _this2.target.offsetHeight; // Set the target's height to auto!
-
-          _this2.target.style.height = _this2.data.height.current + 'px';
-
-          if (_this2.data.open) {
-            // It is no longer open
-            _this2.data.open = false; // Toggle some classes for css usage
-
-            _this2.item.classList.remove("".concat(_this2.settings["class"], "--open")); // Now we need to animate this change
-
-
-            _this2.close();
-          } else {
-            // It is no longer close
-            _this2.data.open = true; // Toggle some classes for css usage
-
-            _this2.item.classList.add("".concat(_this2.settings["class"], "--open")); // Now we need to animate this change
-
-
-            _this2.open();
-          }
-        });
-      }
-    }
-  }, {
-    key: "ease",
-    value: function ease() {
-      // Grab the current time
-      this.time.current = Date.now(); // The amount of time that has passed since the last time we called this function
-
-      this.time.elapsed = this.time.current - this.time.previous; // If the time has been more than our interval
-
-      if (this.time.elapsed >= this.time.interval) {
-        // Reset the previous time var
-        this.time.previous = this.time.current - this.time.elapsed % this.time.interval; // return some carzy easing maths I dont understand ¯\_(ツ)_/¯
-
-        return ((1 - this.data.height.current / this.data.height.full ^ 2) - 1) * this.time.interval / this.settings.duration * this.data.height.full;
-      } // Otherwise return 0
-
-
-      return 0;
-    }
-  }, {
     key: "open",
     value: function open() {
-      var _this3 = this;
+      if (this.active == false) {
+        // Animate the accordion item
+        this.animate(); // No longer active
 
-      window.requestAnimationFrame(function () {
-        // Add an eased amount to our current height
-        _this3.data.height.current += _this3.ease(); // Apply this Math.ceil(height) to our target
-
-        _this3.target.style.height = Math.ceil(_this3.data.height.current) + 'px'; // If it's not already completely open, continue this function
-
-        if (_this3.target.offsetHeight < _this3.data.height.full && _this3.data.open === true) _this3.open();
-      });
+        this.active = true;
+      }
     }
   }, {
     key: "close",
     value: function close() {
-      var _this4 = this;
+      if (this.active) {
+        // Animate the accordion item
+        this.animate(); // No longer active
 
-      window.requestAnimationFrame(function () {
-        // Remove an eased amount to our current height
-        _this4.data.height.current -= _this4.ease(); // Apply this Math.floor(height) to our target
+        this.active = false;
+      }
+    }
+  }, {
+    key: "animate",
+    value: function animate() {
+      var _this2 = this;
 
-        _this4.target.style.height = Math.floor(_this4.data.height.current) + 'px'; // If it's not already completely shut, continue this function
+      // Grab the current height
+      this.height.current = this.target.clientHeight; // Some trickery to grab some height details because we can't animate the 'auto' value
 
-        if (_this4.target.offsetHeight > 0 && _this4.data.open === false) _this4.close();
-      });
+      if (this.active == false) {
+        this.target.style.height = 'auto';
+        this.height["new"] = this.target.clientHeight;
+        this.target.style.height = this.height.current + 'px'; // Also add an active class
+
+        this.element.classList.add('active');
+      } else {
+        // Remove the active class if it's no longer active
+        this.element.classList.remove('active');
+      } // Tween the height of the target element
+
+
+      this.controller.tween({
+        // We will always animate from the current height
+        from: this.height.current,
+        // If it is active, set the height to 0 otherwise set it to full height
+        to: this.active ? 0 : this.height["new"]
+      }, function (value) {
+        return _this2.target.style.height = value + 'px';
+      }, 400);
     }
   }]);
 
--- src/js/accordions.js
+++ src/js/accordions.js
@@ -1,171 +1,144 @@
-/* -------------------------------------------------
-Accordion class to handle the opening/closing
-of any accoridon content!
-------------------------------------------------- */
+/*--------------------------------------------------------------------------------
+Import External Modules
+--------------------------------------------------------------------------------*/
+
+import Tween from '@meteora-digital/tween';
+
+/*--------------------------------------------------------------------------------
+Accordion functionality
+--------------------------------------------------------------------------------*/
 
 export default class Accordion {
-  constructor(block, options = {}) {
-    // The block is the whole AccordionBlock container
-    this.block = block;
-    // Here we can add some custom settings!
+  constructor(section, options = {}) {
+    // The parent container
+    this.section = section;
+    // We will store the accordion items here
+    this.items = [];
+
+    // The user settings / defaults
     this.settings = {
-      class: 'js-accordion',
-      duration: 300, //ms
-      closeOthers: true,
+      multiple: false,
     }
 
-    // ObjectAssign all the user's options
+    // Object assign the settings
     for (let key in this.settings) {
-      // Just check if the key exists in the user's options and if it does override the defaults
-      if (options[key] != undefined) this.settings[key] = options[key];
+      if (this.settings.hasOwnProperty(key) && options.hasOwnProperty(key)) this.settings[key] = options[key];
     }
-
-    // Initialise the accordion block
-    this.init();
   }
 
-  init() {
-    // Ideally we want the elements in an array, not a nodeList
-    this.AccordionItems = [];
-    // Find the item elements. This will be found using this.settings.class followed by --item
-    this.elements = this.block.querySelectorAll(`.${this.settings.class}--item`) || false;
-    // Loop the nodeList and push each element inside of an object to the items array
-    for (let i = 0; i < this.elements.length; i++) this.AccordionItems.push(new AccordionItem(this.elements[i], this.settings));
+  // This will set up an accoridon item and place it into the items array
+  add(element, options = {}) {
+    if (element.nodeType) {
+      const Item = new AccordionItem(element, options);
 
-    // If we wanna close the others when an item opens
-    if (this.settings.closeOthers) {
-      // For each accordion item
-      this.AccordionItems.forEach((AccordionItem) => {
-        try {
-          // Add a click handler
-          AccordionItem.trigger.addEventListener('click', (e) => {
-            e.preventDefault();
-            // Loop all open accordions (not this one!) and close them
-            this.AccordionItems.filter((item) => item.data.open && item.trigger != AccordionItem.trigger).forEach((item) => {
-              item.item.classList.remove(`${this.settings.class}--open`);
-              item.data.open = false;
-              item.close();
-            });
-          });
-        } catch (err) {
-          // We'll probably reach here if we don't have a trigger element
-          console.log(err);
-        }
-      });
-    }
+      // If we have a trigger and a target and they are both elements
+      if (Item.trigger && Item.trigger.nodeType && Item.target && Item.target.nodeType) {
+
+        // Add a click event to the item
+        Item.trigger.addEventListener('click', (e) => {
+          e.preventDefault();
+
+          // If the item is active
+          if (Item.active) {
+            // Close it
+            Item.close();
+          }
+
+          // Otherwise open it, and maybe close the rest
+          else {
+            // If we dont want multiple open at once, close the other items
+            if (this.settings.multiple != true) {
+              this.items.filter((i) => i != Item && i.active).forEach((i) => i.close());
+            }
+
+            // Open the item
+            Item.open();
+          }
+        });
+
+        // Slap this AccordionItem into the items array
+        this.items.push(Item);
+      }
+    };
   }
 }
 
 class AccordionItem {
-  constructor(item, settings) {
-    this.item = item;
+  constructor(element, options = {}) {
+    // Grab the element
+    this.element = element;
+    // This will store some height data for us
+    this.height = {};
+    // The animation controller
+    this.controller = new Tween;
 
-    // Our accordion settings
-    this.settings = settings;
-
-    // Some settings for the item
-    this.data = {
-      open: false,
-      // We'll use this for the animations
-      height: {
-        current: 0,
-        full: 0,
-      }
+    // The user and default settings
+    this.settings = {
+      active: false,
+      trigger: this.element.firstElementChild,
+      target: this.element.lastElementChild,
     }
 
-    // Used for fps control
-    this.time = {
-      previous: Date.now(),
-      current: Date.now(),
-      elapsed: 0,
-      interval: 16.6666666667, // (1000ms / 60 = 16.6666666667ms) = 60fps
+    // Object assign the settings
+    for (let key in this.settings) {
+      if (this.settings.hasOwnProperty(key) && options.hasOwnProperty(key)) this.settings[key] = options[key];
     }
 
-    this.init();
-  }
+    // The element that we will interact with
+    this.trigger = this.settings.trigger;
+    // The element that will open / close
+    this.target = this.settings.target;
+    // An active state for the accordion item
+    this.active = this.settings.active;
 
-  init() {
-    // Find the trigger element
-    this.trigger = this.item.querySelector(`.${this.settings.class}--trigger`) || false;
-    // Find the target element
-    this.target = this.item.querySelector(`.${this.settings.class}--target`) || false;
+    // Remove the settings because we dont need them now
+    delete this.settings;
 
-    // If we have both a trigger and a target
-    if (this.trigger && this.target) {
-      // When we click the trigger
-      this.trigger.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        // Set the item's current height to it's current height
-        this.data.height.current = this.target.offsetHeight;
-
-        // Set the target's height to auto!
-        this.target.style.height = 'auto';
-
-        // Save the target's height
-        this.data.height.full = this.target.offsetHeight;
-
-        // Set the target's height to auto!
-        this.target.style.height = this.data.height.current + 'px';
-
-        if (this.data.open) {
-          // It is no longer open
-          this.data.open = false;
-
-          // Toggle some classes for css usage
-          this.item.classList.remove(`${this.settings.class}--open`);
-
-          // Now we need to animate this change
-          this.close();
-        } else {
-          // It is no longer close
-          this.data.open = true;
-
-          // Toggle some classes for css usage
-          this.item.classList.add(`${this.settings.class}--open`);
-
-          // Now we need to animate this change
-          this.open();
-        }
-      });
-    }
-  }
-
-  ease() {
-    // Grab the current time
-    this.time.current = Date.now();
-    // The amount of time that has passed since the last time we called this function
-    this.time.elapsed = this.time.current - this.time.previous;
-    // If the time has been more than our interval
-    if (this.time.elapsed >= this.time.interval) {
-      // Reset the previous time var
-      this.time.previous = this.time.current - (this.time.elapsed % this.time.interval);
-      // return some carzy easing maths I dont understand ¯\_(ツ)_/¯
-      return ((1 - (this.data.height.current / this.data.height.full)^2) - 1) * this.time.interval / this.settings.duration * this.data.height.full;
-    }
-    // Otherwise return 0
-    return 0;
+    // Initialise the styles
+    if (this.active) this.element.classList.add('active');
+    this.target.style.height = (this.active) ? 'auto' : '0px';
   }
 
   open() {
-    window.requestAnimationFrame(() => {
-      // Add an eased amount to our current height
-      this.data.height.current += this.ease();
-      // Apply this Math.ceil(height) to our target
-      this.target.style.height = Math.ceil(this.data.height.current) + 'px';
-      // If it's not already completely open, continue this function
-      if (this.target.offsetHeight < this.data.height.full && this.data.open === true) this.open();
-    });
+    if (this.active == false) {
+      // Animate the accordion item
+      this.animate();
+      // No longer active
+      this.active = true;
+    }
   }
 
   close() {
-    window.requestAnimationFrame(() => {
-      // Remove an eased amount to our current height
-      this.data.height.current -= this.ease();
-      // Apply this Math.floor(height) to our target
-      this.target.style.height = Math.floor(this.data.height.current) + 'px';
-      // If it's not already completely shut, continue this function
-      if (this.target.offsetHeight > 0 && this.data.open === false) this.close();
-    });
+    if (this.active) {
+      // Animate the accordion item
+      this.animate();
+      // No longer active
+      this.active = false;
+    }
+  }
+
+  animate() {
+    // Grab the current height
+    this.height.current = this.target.clientHeight;
+
+    // Some trickery to grab some height details because we can't animate the 'auto' value
+    if (this.active == false) {
+      this.target.style.height = 'auto';
+      this.height.new = this.target.clientHeight;
+      this.target.style.height = this.height.current + 'px';
+      // Also add an active class
+      this.element.classList.add('active');
+    } else {
+      // Remove the active class if it's no longer active
+      this.element.classList.remove('active');
+    }
+
+    // Tween the height of the target element
+    this.controller.tween({
+      // We will always animate from the current height
+      from: this.height.current,
+      // If it is active, set the height to 0 otherwise set it to full height
+      to: (this.active) ? 0 : this.height.new,
+    }, (value) => this.target.style.height = value + 'px', 400);
   }
 }
